1f98f20d05a68c9394d73ab08441d868c05e7368
Tomas Spirito <tomasspirito10@gmail.com>
2025-11-27 02:57:14 -0300

    translate 2.0

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -9,22 +9,20 @@ export class CoursesController {
 
   @UseGuards(JwtDecodeGuard)
   @Get()
-  findAll(
-    @Query('userId') userId?: string,
-    @Query('status') status?: string
-  ) {
-    if (status === 'available') {
-      if (!userId) {
-        throw new BadRequestException('userId is required to get available courses');
-      }
-      return this.coursesService.findAvailableCoursesForUser(userId);
-    }
-
+  findAll(@Query('userId') userId?: string) {
     if (userId) {
       return this.coursesService.findCoursesForUser(userId);
     }
-
     return this.coursesService.findAll();
+  }
+
+  @UseGuards(JwtDecodeGuard)
+  @Get('available')
+  findAvailable(@Query('userId') userId: string) {
+    if (!userId) {
+      throw new BadRequestException('userId is required to get available courses');
+    }
+    return this.coursesService.findAvailableCoursesForUser(userId);
   }
 
   @Get(':id')
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3002',
+    origin: ['http://localhost:3000', 'http://localhost:3002'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
