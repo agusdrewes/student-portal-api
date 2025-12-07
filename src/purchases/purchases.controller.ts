535c6a7c77f4a155a10577871c9f219e57471be1
delfinapiendii <delfinapiendi@gmail.com>
2025-12-07 15:17:33 -0300

    wallet transfers mine

--- src/purchases/purchases.controller.ts
+++ src/purchases/purchases.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Get, Param, Body, UseGuards } from '@nestjs/common';
 import { PurchasesService } from './purchases.service';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
 import { JwtDecodeGuard } from 'src/auth/jwt-decode.guard';
+import { User } from 'src/auth/user.decorator';
 
 @Controller('users/:userId/purchases')
 export class PurchasesController {
@@ -20,4 +21,15 @@ export class PurchasesController {
   findAll(@Param('userId') userId: string) {
     return this.purchasesService.findByUser(userId);
   }
+
+  @UseGuards(JwtDecodeGuard)
+  @Get('transfers/sync')
+  async syncTransfers(
+    @User('sub') userUuid: string,
+    @User('rawToken') token: string,
+
+  ) {
+    return this.purchasesService.syncTransfers(userUuid, token);
+  }
+
 }
