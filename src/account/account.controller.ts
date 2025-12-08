34370be7c43f2a4e366b7cefb87ec99bf5bdec23
delfinapiendii <delfinapiendi@gmail.com>
2025-12-08 15:01:53 -0300

    wallet sync with core

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards, Req } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { DepositDto } from './dtos/account.dto';
 import { JwtDecodeGuard } from 'src/auth/jwt-decode.guard';
+import { User } from 'src/auth/user.decorator';
 
 @Controller('account')
 export class AccountController {
@@ -18,4 +19,12 @@ export class AccountController {
   deposit(@Param('userId') userId: string, @Body() dto: DepositDto) {
     return this.accountService.deposit(String(userId), dto);
   }
+
+  @Get('wallet/sync')
+    @UseGuards(JwtDecodeGuard)
+    async syncStorePurchases(@User('sub') userUuid: string, @Req() req) {
+      const token = req.headers.authorization?.split(' ')[1];
+      return this.accountService.syncWallet(userUuid, token);
+    }
+   
 }
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -4,9 +4,11 @@ import { Repository } from 'typeorm';
 import { Account } from './entities/account.entity';
 import { User } from '../user/entities/user.entity';
 import { DepositDto } from './dtos/account.dto';
+import axios from 'axios';
 
 @Injectable()
 export class AccountService {
+  
   constructor(
     @InjectRepository(Account)
     private readonly accountRepo: Repository<Account>,
@@ -34,5 +36,44 @@ export class AccountService {
     await this.accountRepo.save(account);
 
     return { balance: account.balance.toFixed(2) };
+  }
+
+  async syncWallet(userUuid: string, token: string) {
+    const user = await this.userRepo.findOne({ where: { id: userUuid } });
+    if (!user) throw new NotFoundException('User not found');
+  
+    const response = await axios.get(
+      'https://jtseq9puk0.execute-api.us-east-1.amazonaws.com/api/wallets/mine',
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    );
+  
+    const wallets = response.data?.data;
+    if (!wallets || wallets.length === 0) {
+      throw new NotFoundException('No wallet returned by CORE');
+    }
+  
+    const coreBalance = parseFloat(wallets[0].balance); 
+  
+    let account = await this.accountRepo.findOne({
+      where: { user: { id: user.id } },
+    });
+  
+    if (!account) {
+      account = this.accountRepo.create({
+        user,
+        balance: coreBalance,
+      });
+    } else {
+      account.balance = coreBalance;
+    }
+  
+    await this.accountRepo.save(account);
+  
+    return {
+      success: true,
+      balance: coreBalance.toFixed(2),
+    };
   }
 }
